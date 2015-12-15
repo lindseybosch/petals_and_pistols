99af57fda2990476fd2929216134d68c3a005d19
Lindsey Bosch <lindseybbosch@gmail.com>
2015-12-14 17:09:02 -0800

    add folder config

---
+++ api/config/environment.js
@@ -0,0 +1,9 @@
+var _ = require('lodash');
+
+var localEnvVars = {
+  TITLE:      'Petals and Pistols',
+  SAFE_TITLE: 'Petals_and_Pistols'
+};
+
+// Merge all environmental variables into one object.
+module.exports = _.extend(process.env, localEnvVars);
