5c08ac51bf3ee6fde0fb4c25e9a1b8e3b7b38255
Lindsey Bosch <lindseybbosch@gmail.com>
2015-12-16 10:24:20 -0800

    add correct app name to module

--- client/js/app/auth.service.js
+++ client/js/app/auth.service.js
@@ -2,7 +2,7 @@
   "use strict";
 
   angular
-    .module("app")
+    .module("petalsAndPistols")
     .factory("authService", authService);
 
   authService.$inject = ["$log", "$http", "tokenService"];
